eb7c265bccc58791e176f30235052ecb829040ac
Bradley Bain <brad_coding@live.com>
2014-04-12 17:54:58 -0500

    initialize the interstitial with an ad id

--- Pasteify/Controllers/CPPStep3ViewController.m
+++ Pasteify/Controllers/CPPStep3ViewController.m
@@ -50,6 +50,9 @@
     manipulatorView.image = [[CPPCurrentProjectService project] foreground];
     
     // Load the interstitial
+    self.interstitial = [MPInterstitialAdController
+                         interstitialAdControllerForAdUnitId:@"058d92d58fec4bf7a1785d97ba8d3328"];
+
 #if DEBUG
     [self.interstitial setTesting:true];
 #endif
